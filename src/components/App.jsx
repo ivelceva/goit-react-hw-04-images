6b5b55ceb3e42629bba415b3ea3a25243d19a4fa
ivelceva <73798374+ivelceva@users.noreply.github.com>
2023-05-20 12:54:56 -0700

    modal_update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,97 @@
-import React from 'react';
-import Gallery from './Gallery';
+import React, { useState, useEffect } from 'react';
+import { getFetch } from '../Fetch';
+import Searchbar from './searchBar/SearchBar';
+import ImageGallery from './gallery/imageGallery/ImageGallery';
+import Loader from './gallery/loader/Loader';
+import Modal from './gallery/modal/Modal';
+import LoadMore from './gallery/button/Button';
 
 const App = () => {
+  const [query, setQuery] = useState(null);
+  const [hits, setHits] = useState([]);
+  const [page, setPage] = useState(1);
+  const perPage = 12;
+  const [totalPages, setTotalPages] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [currentLargeImageURL, setCurrentLargeImageURL] = useState('');
+  const [error, setError] = useState({
+    status: false,
+    message: '',
+  });
+
+  useEffect(() => {
+    const handleFetchHits = async () => {
+      try {
+        setLoading(true);
+
+        const data = await getFetch(query, page, perPage);
+
+        if (data.hits.length === 0) {
+          setError({
+            status: true,
+            message: `Sorry, there are no images matching ${query}. Please try again.`,
+          });
+          return;
+        }
+
+        const totalPages = Math.ceil(data.totalHits / perPage);
+
+        setHits(prevHits => [...prevHits, ...data.hits]);
+        setTotalPages(totalPages);
+      } catch (error) {
+        setError({
+          status: true,
+          message: 'Something went wrong :( Please try again later!',
+        });
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (query && page > 0) {
+      handleFetchHits();
+    }
+  }, [query, page, perPage]);
+
+  const handleLoadMore = () => {
+    setPage(prevPage => prevPage + 1);
+  };
+
+  const handleSubmit = newQuery => {
+    if (newQuery !== query) {
+      setHits([]);
+      setQuery(newQuery);
+      setPage(1);
+      setError({
+        status: false,
+        message: '',
+      });
+    }
+  };
+
+  const toggleModal = url => {
+    setCurrentLargeImageURL(url);
+  };
+
+  const isHits = hits.length > 0;
+  const showError = error.status && !loading;
+  const errorMessage = error.message;
+  const buttonVisible = isHits && page < totalPages && !loading;
+
   return (
-    <div>
-      <Gallery />
-    </div>
+    <>
+      <Searchbar onSubmit={handleSubmit} />
+      {showError && errorMessage}
+      {isHits && <ImageGallery hits={hits} onClick={toggleModal} />}
+      {loading && <Loader />}
+      {buttonVisible && <LoadMore onClick={handleLoadMore} />}
+      {currentLargeImageURL && (
+        <Modal
+          largeImageURL={currentLargeImageURL}
+          onModalClose={() => setCurrentLargeImageURL('')}
+        />
+      )}
+    </>
   );
 };
 
--- src/components/gallery/modal/Modal.jsx
+++ src/components/gallery/modal/Modal.jsx
@@ -17,14 +17,24 @@ const Modal = ({ onModalClose, largeImageURL }) => {
     };
   }, [onModalClose]);
 
+  const handleClick = e => {
+    if (e.currentTarget === e.target) {
+      onModalClose();
+    }
+  };
+
   return (
-    <div className={css.overlay} onClick={onModalClose}>
-      <div className={css.modal}>
-        <img src={largeImageURL} alt="" />
-      </div>
+    <div className={css.overlay} onClick={handleClick}>
+      <ModalContent largeImageURL={largeImageURL} />
     </div>
   );
 };
+
+const ModalContent = ({ largeImageURL }) => (
+  <div className={css.modal}>
+    <img src={largeImageURL} alt="" />
+  </div>
+);
 
 Modal.propTypes = {
   onModalClose: PropTypes.func.isRequired,
@@ -33,38 +43,3 @@ Modal.propTypes = {
 
 export default Modal;
 
-// import { Component } from 'react';
-// import PropTypes from 'prop-types';
-// import css from './Modal.module.css';
-
-// export class Modal extends Component {
-//   componentDidMount() {
-//     window.addEventListener('keydown', this.handleKeyDown);
-//   }
-
-//   componentWillUnmount() {
-//     window.removeEventListener('keydown', this.handleKeyDown);
-//   }
-
-//   handleKeyDown = e => {
-//     if (e.keyCode === 27 || e.currentTarget === e.target) {
-//       return this.props.onModalClose();
-//     }
-//   };
-
-//   render() {
-//     const { largeImageURL } = this.props;
-//     return (
-//       <div className={css.overlay} onClick={this.handleKeyDown}>
-//         <div className={css.modal}>
-//           <img src={largeImageURL} alt="" />
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-// Modal.propTypes = {
-//   onModalClose: PropTypes.func.isRequired,
-//   largeImageURL: PropTypes.string.isRequired,
-// };
